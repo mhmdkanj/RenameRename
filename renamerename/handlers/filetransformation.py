cfdd6c808483635fc73ba3a85f7b518fc5e040e8
Mohamad Kanj <mhmd.kanj@gmail.com>
2022-01-24 17:21:37 +0100

    feat: add reversed method for FileTransformation

--- renamerename/handlers/filetransformation.py
+++ renamerename/handlers/filetransformation.py
@@ -4,20 +4,26 @@ class FileTransformation(MutableMapping):
     def __init__(self, filenames):
         self.transformations = {name: name for name in filenames}
 
+
     def __setitem__(self, key, value):
         self.transformations[key] = value
+
 
     def __getitem__(self, key):
         return self.transformations[key]
 
+
     def __delitem__(self, key):
         del self.transformations[key]
+
 
     def __iter__(self):
         return iter(self.transformations)
 
+
     def __len__(self):
         return len(self.transformations)
+
 
     def __str__(self):
         output = ''
@@ -25,5 +31,20 @@ class FileTransformation(MutableMapping):
             output += k + " ----> " + v + "\n"
         return output
 
+
     def __repr__(self):
         return f"{self.__class__.__name__}({repr(self.transformations)})"
+
+
+    def get_reversed(self):
+        reversed_transformations = {}
+        for k, v in self.transformations.items():
+            reversed_transformations.setdefault(v, []).append(k)
+        return reversed_transformations
+
+
+    def has_duplicates(self):
+        for v in self.get_reversed().values():
+            if len(v) > 1:
+                return True
+        return False
