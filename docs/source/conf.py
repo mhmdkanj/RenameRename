b5b2b4ea7818af4332547b9095ba5ffe57b271c0
Mohamad Kanj <mhmd.kanj@gmail.com>
2022-02-13 22:01:52 +0100

    style: fix docs conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,13 +43,16 @@ autoapi_ignore = ['*test*', '*test_*', '*__main__*', '*app*']
 autoapi_keep_files = False
 autoapi_python_class_content = 'both'
 
+
 def skip(app, what, name, obj, would_skip, options):
     if name == "__init__":
         return False
     return would_skip
 
+
 def setup(app):
     app.connect("autodoc-skip-member", skip)
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
