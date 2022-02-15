3be037d083349110f18a95c0445871f62d73e792
Mohamad Kanj <mhmd.kanj@gmail.com>
2022-02-16 00:46:14 +0100

    chore: release v1.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Mohamad Kanj'
 author = 'Mohamad Kanj'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
